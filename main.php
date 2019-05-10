69a1e63e472f450d717a3f63283704b42a3acc0e
Iishwa <34411538+Iishwa@users.noreply.github.com>
2019-05-10 11:38:21 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,385 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    
+
+  <title>Bootstrap</title>
+  <!--Bootstrap..getting started..option from google ....-->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="css\style.css">
+  <link rel="stylesheet" type="font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css\animate.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  </head>
+
+  <body>
+    <!--<button class="btn btn-primary">Login</button>-->
+    <!--container-fluid takes entire width of the screen-->
+<!--div class is just like a wrapper that holds row and columns-->
+<div id="myNavbar" class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+
+
+      </button>
+      <a href="#" class="navbar-brand">Ishwar Kumar</a>
+
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+      <li><a href="#header">Home</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#pricing">Pricing</a></li>
+      <li><a href="#orders">Orders</a></li>
+      <li><a href="#team">Team</a></li>
+      <li><a href="#client">Client</a></li>
+      <li><a href="#contact">Contact</a></li>
+      </ul>
+     </div>
+
+  </div>
+</div>
+<!--End Navigation-->
+<!--End Navigation-->
+<div id="header" class="header">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 wow bounceInLeft" data-wow-delay="1s">
+        <h1>President</h1>
+        <p class="wow fadeInUp" data-wow-delay="0.4"><font color="white">Ram Nath Kovind (born 1 October 1945) is the 14th and current<br> President of India, in office since 25 July 2017.[1] Previously <br>he had served as the Governor of Bihar from 2015 to 2017<br> and was a Member of Parliament, Rajya Sabha from 1994 to 2006.<br> Kovind was nominated as a presidential candidate<br> by the ruling NDA coalition and won the 2017 presidential election,<br> becoming the second Dalit to be elected to the post of President.</font></p>
+        <button class="btn btn-lg btn-primary">Buy Now</button>
+        <button class="btn btn-lg btn-success">View Details</button>
+</div>
+<div class="col-lg-6 col-md-6 wow bounceInRight" data-wow-delay="1s">
+<img src="images\a3.jpg" alt="">
+</div>
+</div>
+</div></div>
+
+<!--End Header-->
+
+<!--Services-->
+<div id="services" class="services">   
+  <div class="container">
+    <h2 class="wow fadeInUp"><font color="black">Services</font></h2>
+    <p class="wow fadeInUp" data-wow-delay="0.4">New Delhi is the capital of India</p>
+    <div class="row">
+      <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="1.8s" >
+        <i class="fa fa-desktop" aria-hidden="true"></i>
+        <h4>Web Design</h4>
+        <p class="wow fadeInUp" data-wow-delay="0.4">Web design encompasses many different skills and disciplines in the production and maintenance of websites.
+        Please give us a chance </p>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="1.4s">
+        <i class="fa fa-mobile" aria-hidden="true"></i>
+        <h4>Mobile Apps</h4>
+        <p class="wow fadeInUp" data-wow-delay="0.4">A mobile app or mobile application is a computer program designed to run on a mobile device such as a phone/tablet or watch.</p>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="0.8s">
+        
+        <i class="fa fa-database" aria-hidden="true"></i>
+        <h4>Database</h4>
+        <p class="wow fadeInUp" data-wow-delay="0.4">The database management system (DBMS) is the software that interacts with end users, applications, and the database itself to capture and analyze data.</p>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="0.4s">
+        
+        <i class="fa fa-university" aria-hidden="true"></i>
+        <h4>Consulting</h4>
+        <p class="wow fadeInUp" data-wow-delay="0.4">To ensure you gain full market access, your goods must comply with the requirements of your target market. Identifying and meeting those requirements is a complex challenge.</p>
+      </div>
+
+
+    </div>
+    
+
+  </div>
+
+
+</div>
+<!--End Services-->
+<!--Pricing-->
+<div id="pricing" class="pricing">   
+  <div class="container">
+    <div class="row">
+      <h2 class="wow fadeInUp">Pricing</h4>
+        <p>Following are the pricing we used to provide it to our customers.</p>
+      <div class="col-lg-3 col-md-3 wow flipInY" data-wow-delay="0.8s">
+        <div class="packages">
+          <h4>Bronze</h4>
+          <h1>9.99</h1>
+          <b>Monthly</b>
+          <p class="wow fadeInUp" data-wow-delay="0.4">With desktop application uh can use the following requirements</p>
+          <hr color="brown">
+          <li>100 User</li>
+          <li>SSL Certificate</li>
+          <li>Online Support</li>
+          <li>100 Email Address</li>
+          <li>300 gb Diskspace</li>
+          <li>MySQl Database</li>
+          <button class="btn btn-success">Get Started</button>
+        </div>
+      </div>
+ <div class="col-lg-3 col-md-3 wow flipInY" data-wow-delay="1.2s">
+        <div class="packages">
+          <h4>Silver</h4>
+          <h1>19.99</h1>
+          <b>Monthly</b>
+          <p class="wow fadeInUp" data-wow-delay="0.4">With desktop application uh can use the following requirements</p>
+          <hr color="brown">
+          <li>100 User</li>
+          <li>SSL Certificate</li>
+          <li>Online Support</li>
+          <li>100 Email Address</li>
+          <li>300 gb Diskspace</li>
+          <li>MySQl Database</li>
+          <button class="btn btn-success">Get Started</button>
+        </div>
+      </div>
+<div class="col-lg-3 col-md-3 wow flipInY" data-wow-delay="1.6s">
+        <div class="packages">
+          <h4>Gold</h4>
+          <h1>29.99</h1>
+          <b>Monthly</b>
+          <p class="wow fadeInUp" data-wow-delay="0.4">With desktop application uh can use the following requirements</p>
+          <hr color="brown">
+          <li>100 User</li>
+          <li>SSL Certificate</li>
+          <li>Online Support</li>
+          <li>100 Email Address</li>
+          <li>300 gb Diskspace</li>
+          <li>MySQl Database</li>
+          <button class="btn btn-success">Get Started</button>
+        </div>
+      </div>
+<div class="col-lg-3 col-md-3 wow flipInY" data-wow-delay="2s">
+        <div class="packages">
+          <h4>Premium</h4>
+          <h1>39.99</h1>
+          <b>Monthly</b>
+          <p class="wow fadeInUp" data-wow-delay="0.4">With desktop application uh can use the following requirements</p>
+          <hr color="brown">
+          <li>100 User</li>
+          <li>SSL Certificate</li>
+          <li>Online Support</li>
+          <li>100 Email Address</li>
+          <li>300 gb Diskspace</li>
+          <li>MySQl Database</li>
+          <button class="btn btn-success">Get Started</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--End Pricing-->
+<div id="orders" class="orders" >
+  <div class="container">
+    <div class="row">
+      <h2 class="wow fadeInUp">Our trusted clients</h2>
+      <p class="wow fadeInUp" data-wow-delay="0.4s">We have collborated with large companies and serve them with trust</p>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+        <li><img src="images\c1.jpg" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+        <li><img src="images\c2.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+        <li><img src="images\c3.jpg" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+       <li> <img src="images\c4.jpg" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c5.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c6.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c7.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c8.jpg" height=200 width=300  alt=""></li>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Start Team-->
+<div id="team" class="team" >
+  <div class="container">
+    <div class="row">
+      <h2 class="wow fadeInUp">Meet Our Teams</h2>
+      <p class="wow fadeInUp" data-wow-delay="0.4">The advanced business and technology applied company</p>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="1.8s">
+        <img src="images\3.jpg" height=200 width=300  alt="">
+        <h4>Ishwar Kumar</h4>
+        <b>CEO and Founder</b>
+        <p>khv vhtdcyt kj hv bdthrhfh jhfcytfdg.liukbhvdhrtgsdtg kjhbdfytvr ghfcscr yvrr</p>
+<a href="https://www.facebook.com/"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href="www.twitter.com"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+<a href="www.linkdin.com"> <i class="fa fa-linkedin" aria-hidden="true"></i></a>
+<a href="www.pinterest.com"> <i class="fa fa-pinterest" aria-hidden="true"></i></a>
+      </div>
+<div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="1.4s">
+        <img src="images\4.jpg" height=200 width=300 alt="">
+        <h4>Ishwar Kumar</h4>
+        <b>CEO and Founder</b>
+        <p class="wow fadeInUp" data-wow-delay="0.4">khv vhtdcyt kj hv bdthrhfh jhfcytfdg.liukbhvdhrtgsdtg kjhbdfytvr ghfcscr yvrr</p>
+<a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-linkedin" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-pinterest" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="1.2s">
+        <img src="images\5.jpg" height=200 width=300 alt="">
+        <h4>Ishwar Kumar</h4>
+        <b>CEO and Founder</b>
+        <p class="wow fadeInUp" data-wow-delay="0.4">khv vhtdcyt kj hv bdthrhfh jhfcytfdg.liukbhvdhrtgsdtg kjhbdfytvr ghfcscr yvrr</p>
+<a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-linkedin" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-pinterest" aria-hidden="true"></i></a>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft" data-wow-delay="0.8s">
+        <img src="images\16.jpg" height=200 width=300 alt="">
+        <h4>Ishwar Kumar</h4>
+        <b>CEO and Founder</b>
+        <p class="wow fadeInUp" data-wow-delay="0.4">khv vhtdcyt kj hv bdthrhfh jhfcytfdg.liukbhvdhrtgsdtg kjhbdfytvr ghfcscr yvrr</p>
+<a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-linkedin" aria-hidden="true"></i></a>
+<a href="#"> <i class="fa fa-pinterest" aria-hidden="true"></i></a>
+      </div>
+
+    </div>
+
+
+  </div>
+
+</div>
+<!--Team section ends here-->
+<!-- Clients section statrs from here--> 
+<div id="client" class="client" >
+  <div class="container">
+    <div class="row">
+      <h2 class="wow fadeInUp">Our trusted clients</h2>
+      <p class="wow fadeInUp" data-wow-delay="0.4s">We have collborated with large companies and served them with trust</p>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+        <li><img src="images\c1.jpg" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+        <li><img src="images\c2.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+        <li><img src="images\c3.jpg" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInLeft">
+       <li> <img src="images\c4.jpg" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c5.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c6.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c7.png" height=200 width=300  alt=""></li>
+      </div>
+      <div class="col-lg-3 col-md-3 wow fadeInRight">
+        <li><img src="images\c8.jpg" height=200 width=300  alt=""></li>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Contact starting---->
+<div id="contact" class="contact">
+  <div class="container">
+    <div class="row">
+      <h2 class="wow fadeInUp">Contact</h2>
+      <p class="wow fadeInUp" >Please contact us or write to us with the help of following information.</p>
+      <div class="col-lg-6 col-md-6">
+        <form action="" method="post">
+        <div class="input-group input-group-lg wow fadeInUp" data-wow-delay="0.4s">
+          <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
+          <input type="text" name="fullname" class="form-control" aria-describedby="sizing-addon1" placeholder="Full Name" required="required">
+        </div>
+      
+      <div class="input-group input-group-lg wow fadeInUp" data-wow-delay="0.8s">
+          <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+          <input type="text" name="email" class="form-control" aria-describedby="sizing-addon1" placeholder="Email" required="required">
+        </div>
+        
+      <div class="input-group input-group-lg wow fadeInUp" data-wow-delay="1.2s">
+          <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-mobile" aria-hidden="true"></i></span>
+          <input type="text" name="contacts" class="form-control" aria-describedby="sizing-addon1" placeholder="Contact" required="required">
+        </div>
+
+    </div>
+    <div class="col-lg-6 col-md-6">
+      <div class="input-group wow fadeInUp"data-wow-delay="1.6s">
+        <textarea  name="msg" id="" cols="80" rows="6" class="form-control"></textarea>
+
+
+      </div>
+      <div class="g-recaptcha" data-sitekey="6Ld3KmQUAAAAAPt71O1D6_qEKvpJF3PQoU9l3CSp"></div><br>
+                        <input type="hidden" value="" id="url" name="url">
+      <button class="btn btn-lg wow fadeInUp" name="submit">Submit</button>
+
+<?php include("insert.php"); ?>
+</div></form>
+
+
+    </div>
+    
+
+  </div>
+</div></div>
+<!---Footer starts-->
+<div id="footer" class="footer">
+ <div class="container">
+   <div class="row">
+     <div class="col-lg-4 col-md-4">
+       <h4 class="wow fadeInUp">Contact Us</h4>
+       <p><i class="fa fa-home" aria-hidden="true"></i>&nbspM6-PCRT Boys Hostel,Bhanpur,Bhopal-462037</p>
+       <p><i class="fa fa-envelope" aria-hidden="true"></i>&nbspishwarkkumar@gmail.com</p>
+       <p><i class="fa fa-mobile" aria-hidden="true"></i>&nbsp+91 8963935541</p>
+       <p><i class="fa fa-globe" aria-hidden="true"></i>&nbspishwarkkumar@gmail.com</p>
+     </div>
+     <div class="col-lg-4 col-md-4">
+       <h4 class="wow fadeInUp">About Us</h4>
+       <p><i class="fa fa-square-o" aria-hidden="true"></i>&nbspAbout Us</p>
+       <p><i class="fa fa-square-o" aria-hidden="true"></i>&nbspPrivacy</p>
+       <p><i class="fa fa-square-o" aria-hidden="true"></i>&nbspTerms & Conditions</p>
+     </div>
+     <div class="col-lg-4 col-md-4">
+      <h4 class="wow fadeInUp">Stay in touch</h4>
+      <a href="https://www.facebook.com/"> <i class="social fa fa-facebook" aria-hidden="true"></i></a>
+       <i class="social fa fa-twitter" aria-hidden="true"></i>
+       <i class="social fa fa-linkedin" aria-hidden="true"></i>
+       <i class="social fa fa-pinterest" aria-hidden="true"></i>
+       <i class="social fa fa-youtube" aria-hidden="true"></i>
+       <i class="social fa fa-github" aria-hidden="true"></i><br>
+        <input type="Email" placeholder="Subscribe For Update..">&nbsp<button class="btn btn-success">Subscribe</button>
+
+     </div>
+   </div>
+ </div>
+</div>
+<script src="js/wow.min.js"></script>
+<script>
+new WOW().init();
+</script>
+
+</body>
+</html>
