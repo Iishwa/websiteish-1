869d57c284cbfc6313db2dfc8a23cb4ccd0e3c54
developer1 <ishwark377@gmail.com>
2024-02-10 11:38:10 +0530

    Added Title to My Company

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
     
     
 
-  <title>Bootstrap</title>
+  <title>My Company</title>
   <!--Bootstrap..getting started..option from google ....-->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
